e856ef804592b9cfbdf40657e0ed6c81f339164f
Gurjeet Singh <devgurjeet@users.noreply.github.com>
2021-01-04 16:07:01 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url, include
+from django.conf.urls import url, include, patterns
 from aldryn_django.utils import i18n_patterns
 import aldryn_addons.urls
-
 
 urlpatterns = [
     # add your own patterns here
 ] + aldryn_addons.urls.patterns() + i18n_patterns(
     # add your own i18n patterns here
-    *aldryn_addons.urls.i18n_patterns()  # MUST be the last entry!
-)
+    *aldryn_addons.urls.i18n_patterns(),  # MUST be the last entry!
+ )
