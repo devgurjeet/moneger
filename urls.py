7f156ff1916d9405e2b992e6dfde905352752a97
Gurjeet Singh <devgurjeet@users.noreply.github.com>
2021-01-04 14:58:42 +0530

    Update urls.py
    Fix url pattern

--- urls.py
+++ urls.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url, include, path
+from django.conf.urls import url, include, patterns
 from aldryn_django.utils import i18n_patterns
 import aldryn_addons.urls
 
 urlpatterns = [
-   path('admin/', admin.site.urls),
-] + aldryn_addons.urls.patterns() 
-
+   
+] + aldryn_addons.urls.patterns() + patterns(
+    # add your own i18n patterns here
+    *aldryn_addons.urls.i18n_patterns()  # MUST be the last entry!
+)
