04490e0427d4e07359c280878067d16425a88794
Gurjeet Singh <devgurjeet@users.noreply.github.com>
2021-01-04 16:36:08 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url, include
-from django.urls import path
+#from django.urls import path
 
 from aldryn_django.utils import i18n_patterns
 import aldryn_addons.urls
 
 api_urls = [
-    path('todos/', include('todos.urls')),
-    path('', include('users.urls')),
+    url('todos/', include('todos.urls')),
+    url('', include('users.urls')),
 ]
 
 urlpatterns = [
-    path('api/', include(api_urls)),
+    url('api/', include(api_urls)),
 ] + aldryn_addons.urls.patterns() + i18n_patterns(
     # add your own i18n patterns here
     *aldryn_addons.urls.i18n_patterns(),  # MUST be the last entry!
