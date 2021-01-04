56121069c6847b6b9300771911b5aaef7cd751cc
Sasan, Gurjeet <developer.gurjeet@gmail.com>
2021-01-04 18:06:28 +0530

    Aadd home template.

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url, include
-#from django.urls import path
+from django.views.generic.base import TemplateView
 
 from aldryn_django.utils import i18n_patterns
 import aldryn_addons.urls
@@ -12,6 +12,8 @@ api_urls = [
 
 urlpatterns = [
     url('api/', include(api_urls)),
+    path('', TemplateView.as_view(template_name="home.html"), name="home")
+
 ] + aldryn_addons.urls.patterns() + i18n_patterns(
     # add your own i18n patterns here
     *aldryn_addons.urls.i18n_patterns(),  # MUST be the last entry!
