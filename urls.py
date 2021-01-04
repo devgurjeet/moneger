89d1c056811bf00aec31bd5dbdd20aa9e7c83539
Sasan, Gurjeet <developer.gurjeet@gmail.com>
2021-01-04 16:27:42 +0530

    Added new apps.

--- urls.py
+++ urls.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url, include
+from django.conf.urls import url, include, path
 from aldryn_django.utils import i18n_patterns
 import aldryn_addons.urls
 
+api_urls = [
+    path('todos/', include('todos.urls')),
+    path('', include('users.urls')),
+]
 
 urlpatterns = [
-    # add your own patterns here
+    path('api/', include(api_urls)),
 ] + aldryn_addons.urls.patterns() + i18n_patterns(
     # add your own i18n patterns here
     *aldryn_addons.urls.i18n_patterns(),  # MUST be the last entry!
