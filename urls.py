dec2382e9d848e45d7703fba027bd35afee69663
Gurjeet Singh <devgurjeet@users.noreply.github.com>
2021-01-04 14:53:40 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url, include
+from django.conf.urls import url, include, path
 from aldryn_django.utils import i18n_patterns
 import aldryn_addons.urls
 
 urlpatterns = [
-    # add your own patterns here
+   path('admin/', admin.site.urls),
 ] + aldryn_addons.urls.patterns() 
 
