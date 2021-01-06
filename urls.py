f5ccd0b54e62c36e6bb7a455d3a399fd79d1f069
Gurjeet Singh <devgurjeet@users.noreply.github.com>
2021-01-06 15:53:58 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -12,12 +12,15 @@ api_urls = [
 ]
 
 urlpatterns = [
-    url('api/', include(api_urls)),
     path('', TemplateView.as_view(template_name="home.html"), name="home"),
 
 ] + aldryn_addons.urls.patterns() + i18n_patterns(
     # add your own i18n patterns here
     *aldryn_addons.urls.i18n_patterns(),  # MUST be the last entry!
  )
+
+urlpatterns += [
+    url('api/', include(api_urls)),
+]
 
 
